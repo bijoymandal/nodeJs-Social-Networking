ebe6f22354794d2208df9499ff240da49cb80795
bijoymandal <bijoy.mandal@mydevteam.com>
2025-10-12 12:29:25 +0530

    project initilization

---
+++ index.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import express from "express";
+import cors from "cors";
+
+const server = express();
+//middleware to parse json request body
+server.use(express.json());
+//CROS Policy configuration
+var corsOptions = {
+    origin:"*"
+}
+server.use(cors(corsOptions));
+
+//error haandler middleware
+server,use((error,req,res,next)=>{
+    console.log(error);
+    res.status(503).send("Something went wrong,please try later");
+});
+
+//default request handler
+server.get("/",(req,res)=>{
+    res.send("Welcome to Social Media Platform");
+});
+
+server.listen(3200,()=>{
+    console.log("server is running at 3200");
+});
