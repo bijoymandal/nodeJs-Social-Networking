140ff6a2ebafd166f9932a27b76c1f9ba1b5b84f
bijoymandal <bijoy.mandal@mydevteam.com>
2025-10-18 11:34:44 +0530

    node modules swagger install

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 import loggerMiddleware from "./src/middleware/logger.middleware.js";
-import fs from 'fs';
 import express from "express";
 import cors from "cors";
 
@@ -11,7 +10,9 @@ import jwtAuth from './src/middleware/jwt.middleware.js';
 import postRouter from "./src/feature/post/routes/post.routes.js";
 import commentRouter from "./src/feature/comments/routes/comments.routes.js";
 import likeRouter from "./src/feature/likes/routes/like.routes.js";
+import { ApplicationError } from "./src/error-handler/applicationError.js";
 
+const port = process.env.PORT || 3200;
 const server = express();
 //middleware to parse json request body
 server.use(express.json());
@@ -26,6 +27,10 @@ server.use(loggerMiddleware);
 //error haandler middleware
 server.use((error,req,res,next)=>{
     console.log(error);
+    if(error instanceof ApplicationError)
+    {
+      res.status(error.code).send(error.message);  
+    }
     res.status(503).send("Something went wrong,please try later");
 });
 
@@ -38,6 +43,6 @@ server.use("/api/users",userRouter);
 server.use("/api/posts",jwtAuth,postRouter);
 server.use("/api/comment",jwtAuth,commentRouter);
 server.use("/api/likes",jwtAuth,likeRouter);
-server.listen(process.env.PORT,()=>{
-    console.log(`server is running at ${process.env.PORT}`);
+server.listen(port,()=>{
+    console.log(`server is running at ${port}`);
 });
